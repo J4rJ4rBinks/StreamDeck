d923adaa80872fec14c0edb26fdf8b4a62502121
J4rJ4rBinks <50147212+J4rJ4rBinks@users.noreply.github.com>
2020-09-03 16:01:02 -0500

    Update actualScript.bat

--- actualScript.bat
+++ actualScript.bat
@@ -1,5 +1,5 @@
 PUSHD "C:\Program Files (x86)\Actual Window Manager"
-START ActualWindowManagerCenter.exe RESTORE "\\ntidlabs\nlc system files$\Lab\Scripts\Video_Effects\Actual Configure.zip"
+START ActualWindowManagerCenter.exe RESTORE "\insert your file directory here\Actual Configure.zip"
 :: Register the program 
 ActualWindowManagerCenter.exe R "insert your username of Actual Window Manager" ID number insert here 
 exit /b
