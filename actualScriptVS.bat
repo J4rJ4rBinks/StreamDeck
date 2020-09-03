77f204f1732f136371c0a5f1b8a686c5876aa53b
J4rJ4rBinks <50147212+J4rJ4rBinks@users.noreply.github.com>
2020-09-03 15:56:45 -0500

    Delete actualScriptVS.bat

--- actualScriptVS.bat
+++
@@ -1,7 +0,0 @@
-PUSHD "C:\Program Files (x86)\Actual Window Manager"
-START ActualWindowManagerCenter.exe RESTORE "\\ntidlabs\nlc system files$\Lab\Scripts\Video_Effects\Actual Configure.zip"
-:: Register the program 
-ActualWindowManagerCenter.exe R "Wesley Blue" AIM805BAD49F0A534D735169A713FF477C7CA3E511D41
-exit /b
-
-
